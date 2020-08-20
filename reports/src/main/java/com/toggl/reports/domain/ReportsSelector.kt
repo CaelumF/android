a43cea52eedf148eae4791423cdbd969f8e1b873
Daivid S. Silverio <daivid.silverio@gmail.com>
2020-08-19 20:01:22 -0400

    🔨 Add logic to selector to create bar chart data

--- reports/src/main/java/com/toggl/reports/domain/ReportsSelector.kt
+++ reports/src/main/java/com/toggl/reports/domain/ReportsSelector.kt
@@ -1,12 +1,20 @@
 package com.toggl.reports.domain
 
+import com.toggl.api.models.Resolution
 import com.toggl.architecture.Failure
 import com.toggl.architecture.Loadable
 import com.toggl.architecture.core.Selector
+import com.toggl.common.extensions.formatForDisplayingDate
+import com.toggl.common.feature.extensions.totalHours
 import com.toggl.common.feature.formatting.DurationFormatter
 import com.toggl.reports.models.ReportData
+import com.toggl.reports.models.TotalsReport
 import java.time.Duration
+import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
+import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -69,6 +77,32 @@ class ReportsSelector @Inject constructor(
             yield(ReportsViewModel.Amounts(amounts = amounts))
         }
 
+        // Bar chart
+        val upperBarLimit = totalsReport.groups.maxOf { it.total }.totalHours
+        val bars = totalsReport.groups
+            .map { group ->
+                Bar(
+                    group.billable.totalHours.toDouble() / upperBarLimit,
+                    group.total.totalHours.toDouble() / upperBarLimit
+                )
+            }
+
+        val startDate = state.localState.dateRange.start
+        val endDate = state.localState.dateRange.end ?: startDate
+        val xLabels = convertReportTimeEntriesToXAxisLabels(totalsReport, startDate, endDate)
+        val yLabels = convertReportTimeEntriesToYAxisLabels(totalsReport, durationFormatter)
+
+        yield(
+            ReportsViewModel.BarChart(
+                BarChartInfo(
+                    bars,
+                    bars.maxOf { bar -> bar.totalValue },
+                    xLabels,
+                    yLabels
+                )
+            )
+        )
+
         // Donut Chart Slices
         val slices = mutableListOf<DonutSlice>()
         val legendInfo = mutableListOf<ReportsViewModel.DonutChartLegend>()
@@ -107,4 +141,38 @@ class ReportsSelector @Inject constructor(
 
         yield(ReportsViewModel.AdvancedReportsOnWeb)
     }.toList()
+
+    private fun convertReportTimeEntriesToXAxisLabels(
+        report: TotalsReport,
+        startDate: OffsetDateTime,
+        endDate: OffsetDateTime
+    ): List<String> {
+        if (report.resolution == Resolution.Day && report.groups.size <= 7)
+            return (0L..report.groups.size).map {
+                val date = startDate.plusDays(it)
+                val dateString = date.formatForDisplayingDate()
+                val dayOfWeekString = date.dayOfWeek.getDisplayName(TextStyle.NARROW, Locale.getDefault())
+                "$dateString\n$dayOfWeekString"
+            }
+
+        if (report.groups.size == 1)
+            return listOf(startDate.formatForDisplayingDate())
+
+        return listOf(startDate.formatForDisplayingDate(), endDate.formatForDisplayingDate())
+    }
+
+    private fun convertReportTimeEntriesToYAxisLabels(report: TotalsReport, durationFormatter: DurationFormatter): BarChartYLabels {
+        fun createLabels(maxValue: Duration, unit: ChronoUnit) =
+            BarChartYLabels(
+                durationFormatter.formatRoundingWithTimeUnit(maxValue, unit),
+                durationFormatter.formatRoundingWithTimeUnit (maxValue.dividedBy(2L), unit),
+                durationFormatter.formatRoundingWithTimeUnit (Duration.ZERO, unit)
+            )
+        val maxTime = report.groups.maxOf { it.total }
+        return when {
+            maxTime.totalHours > 1 -> createLabels(maxTime, ChronoUnit.HOURS)
+            maxTime.toMinutes() > 1 -> createLabels(maxTime, ChronoUnit.MINUTES)
+            else -> createLabels(maxTime, ChronoUnit.SECONDS)
+        }
+    }
 }
