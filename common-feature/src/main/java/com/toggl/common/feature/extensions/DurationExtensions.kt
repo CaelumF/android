a2598457e9d1b2cb14567928baab84d25ab365d1
Daivid S. Silverio <daivid.silverio@gmail.com>
2020-08-19 20:00:33 -0400

    ✨ Add ext. props for total hours and minutes to duration

--- common-feature/src/main/java/com/toggl/common/feature/extensions/DurationExtensions.kt
+++ common-feature/src/main/java/com/toggl/common/feature/extensions/DurationExtensions.kt
@@ -1,5 +1,6 @@
 package com.toggl.common.feature.extensions
 
+import com.toggl.common.Constants
 import java.time.Duration
 import java.util.concurrent.TimeUnit
 
@@ -11,3 +12,9 @@ fun Duration.formatForDisplaying(): String {
         TimeUnit.MILLISECONDS.toSeconds(durationMillis) % TimeUnit.MINUTES.toSeconds(1)
     )
 }
+
+val Duration.totalHours: Float
+    get() = (seconds.toFloat() / Constants.ClockMath.secondsInAnHour)
+
+val Duration.totalMinutes: Float
+    get() = (seconds.toFloat() / Constants.ClockMath.secondsInAMinute)
---
+++ common-feature/src/test/java/com/toggl/common/feature/extensions/DurationExtensionsKtTest.kt
@@ -0,0 +1,29 @@
+package com.toggl.common.feature.extensions
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.time.Duration
+
+internal class DurationExtensionsKtTest {
+
+    @Test
+    fun totalHours() {
+        Duration.ofHours(1).totalHours.shouldBe(1f)
+        Duration.ofHours(2).totalHours.shouldBe(2f)
+        Duration.ofHours(3).totalHours.shouldBe(3f)
+        Duration.ofHours(1).plusMinutes(30).totalHours.shouldBe(1.5f)
+        Duration.ofSeconds(360).totalHours.shouldBe(0.1f)
+    }
+
+    @Test
+    fun totalMinutes() {
+        Duration.ofHours(1).totalMinutes.shouldBe(60f)
+        Duration.ofHours(2).totalMinutes.shouldBe(120f)
+        Duration.ofHours(3).totalMinutes.shouldBe(180f)
+        Duration.ofHours(1).plusMinutes(30).totalMinutes.shouldBe(90f)
+        Duration.ofSeconds(360).totalMinutes.shouldBe(6f)
+        Duration.ofSeconds(90).totalMinutes.shouldBe(1.5f)
+    }
+}
